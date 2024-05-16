844c724270356e2888507c638d1abbda883439ca
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-05-16 20:32:27 +0200

    added few other files

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function LandingPage() {
+import Image from 'next/image';
+
+import HomeBanner from 'public/Home_Banner.png';
+
+export default function body() {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className=" flex h-[600px] w-[100vw] flex-col items-center justify-center gap-2 overflow-hidden text-center">
+      <Image src={HomeBanner} alt="Home Banner" className="h-[100vh] w-[100vw] " />
     </div>
   );
 }
