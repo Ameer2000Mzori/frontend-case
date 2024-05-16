cd2ecc7f32fe5030bf07ec98225c330d2624798b
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-05-16 19:04:58 +0200

    added more styling

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -4,15 +4,15 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="bg-gradient-radial-right flex h-[80px] w-[100vw] flex-col items-center  text-center ">
-      <div className="flex h-[100%] w-[65%]  flex-row items-center justify-between  text-center">
+    <div className="header-bg-gradient-radial-right flex h-[80px] w-[100vw] flex-col items-center  text-center ">
+      <div className="flex h-[100%] w-[75%]  flex-row items-center justify-between  text-center">
         <Link href="/"></Link>
         <div className=" flex flex-row gap-[32px] leading-[24px] text-white">
           <Link href="/home">
-            <p className=" font-Barlow ">Home</p>
+            <p className=" nav-text ">Home</p>
           </Link>
           <Link href="/blog">
-            <p>About</p>
+            <p className="nav-text">About</p>
           </Link>
         </div>
       </div>
