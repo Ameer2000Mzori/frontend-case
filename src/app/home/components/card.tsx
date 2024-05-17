735bb2cc098d4b09c58f6b9e3912ce52d8eda10c
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-05-17 15:05:21 +0200

    added the card data inside the card file

--- src/app/home/components/card.tsx
+++ src/app/home/components/card.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
-export default function card() {
-  return <div>card</div>;
+import Image from 'next/image';
+
+import cardImg from 'public/card_Img.png';
+
+export default function Card() {
+  return (
+    <div className=" flex h-[440px] w-[349px] cursor-pointer flex-col  gap-[24px] text-start">
+      <div className=" relative h-[240px] w-[100%] overflow-hidden rounded-[6px] bg-black">
+        <Image src={cardImg} alt="card image" className=" -z-10 h-[100%] w-[100%] object-cover" />
+        <div className=" absolute bottom-[8px] left-[8px] flex h-[32px] w-[91px] flex-col items-center justify-center rounded-[4px] bg-[#EFEFF8] text-center ">
+          <p className=" mb-[4px] ml-[16px] mr-[16px] mt-[4px] text-[12px] font-medium leading-[24px] text-[#0E1527]">
+            INTERVIEW
+          </p>
+        </div>
+      </div>
+
+      <div className=" flex flex-col items-start justify-between gap-[14px] text-start">
+        <h2 className="h-[50px] w-[100%] font-Barlow text-[21px] font-bold leading-[23px]">
+          Werken bij Social Brothers, volgens developer Ryence
+        </h2>
+
+        <p className="h-[72px] w-[100%] text-[16px] leading-[19px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent luctus velit id ex
+          vestibulum, in tristique risus tincidunt.
+        </p>
+
+        <button>Lees meer =</button>
+      </div>
+    </div>
+  );
 }
--- src/app/home/components/cards.tsx
+++ src/app/home/components/cards.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import Image from 'next/image';
-
-import cardImg from 'public/card_Img.png';
+import Card from './card';
 
 export default function Cards() {
   return (
@@ -12,87 +10,7 @@ export default function Cards() {
           De nieuwste blogs
         </h1>
         <div className=" flex flex-row items-center justify-center gap-[31px] text-center">
-          <div className=" flex h-[440px] w-[349px] cursor-pointer flex-col  gap-[24px] text-start">
-            <div className=" relative h-[240px] w-[100%] overflow-hidden rounded-[6px] bg-black">
-              <Image
-                src={cardImg}
-                alt="card image"
-                className=" -z-10 h-[100%] w-[100%] object-cover"
-              />
-              <div className=" absolute bottom-[8px] left-[8px] flex h-[32px] w-[91px] flex-col items-center justify-center rounded-[4px] bg-[#EFEFF8] text-center ">
-                <p className=" mb-[4px] ml-[16px] mr-[16px] mt-[4px] text-[12px] font-medium leading-[24px] text-[#0E1527]">
-                  INTERVIEW
-                </p>
-              </div>
-            </div>
-
-            <div className=" flex flex-col items-start justify-between gap-[14px] text-start">
-              <h2 className="h-[50px] w-[100%] font-Barlow text-[21px] font-bold leading-[23px]">
-                Werken bij Social Brothers, volgens developer Ryence
-              </h2>
-
-              <p className="h-[72px] w-[100%] text-[16px] leading-[19px]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent luctus velit id ex
-                vestibulum, in tristique risus tincidunt.
-              </p>
-
-              <button>Lees meer =</button>
-            </div>
-          </div>
-          <div className=" flex h-[440px] w-[349px] cursor-pointer flex-col  gap-[24px] text-start">
-            <div className=" relative h-[240px] w-[100%] overflow-hidden rounded-[6px] bg-black">
-              <Image
-                src={cardImg}
-                alt="card image"
-                className=" -z-10 h-[100%] w-[100%] object-cover"
-              />
-              <div className=" absolute bottom-[8px] left-[8px] flex h-[32px] w-[91px] flex-col items-center justify-center rounded-[4px] bg-[#EFEFF8] text-center ">
-                <p className=" mb-[4px] ml-[16px] mr-[16px] mt-[4px] text-[12px] font-medium leading-[24px] text-[#0E1527]">
-                  INTERVIEW
-                </p>
-              </div>
-            </div>
-
-            <div className=" flex flex-col items-start justify-between gap-[14px] text-start">
-              <h2 className="h-[50px] w-[100%] font-Barlow text-[21px] font-bold leading-[23px]">
-                Werken bij Social Brothers, volgens developer Ryence
-              </h2>
-
-              <p className="h-[72px] w-[100%] text-[16px] leading-[19px]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent luctus velit id ex
-                vestibulum, in tristique risus tincidunt.
-              </p>
-
-              <button>Lees meer =</button>
-            </div>
-          </div>
-          <div className=" flex h-[440px] w-[349px] cursor-pointer flex-col  gap-[24px] text-start">
-            <div className=" relative h-[240px] w-[100%] overflow-hidden rounded-[6px] bg-black">
-              <Image
-                src={cardImg}
-                alt="card image"
-                className=" -z-10 h-[100%] w-[100%] object-cover"
-              />
-              <div className=" absolute bottom-[8px] left-[8px] flex h-[32px] w-[91px] flex-col items-center justify-center rounded-[4px] bg-[#EFEFF8] text-center ">
-                <p className=" mb-[4px] ml-[16px] mr-[16px] mt-[4px] text-[12px] font-medium leading-[24px] text-[#0E1527]">
-                  INTERVIEW
-                </p>
-              </div>
-            </div>
-
-            <div className=" flex flex-col items-start justify-between gap-[14px] text-start">
-              <h2 className="h-[50px] w-[100%] font-Barlow text-[21px] font-bold leading-[23px]">
-                Werken bij Social Brothers, volgens developer Ryence
-              </h2>
-
-              <p className="h-[72px] w-[100%] text-[16px] leading-[19px]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent luctus velit id ex
-                vestibulum, in tristique risus tincidunt.
-              </p>
-
-              <button>Lees meer =</button>
-            </div>
-          </div>
+          <Card />
         </div>
       </div>
     </div>
