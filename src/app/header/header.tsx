ae73e05a84e5f3d836fa51a35382adcdee71d1e5
Ameer Ameen <ameerapex496@gmail.com>
2024-05-17 22:23:15 +0200

    fixed logo

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -7,7 +7,9 @@ export default function Header() {
     <div className="header-bg-gradient-radial-right flex h-[80px]  flex-col items-center  text-center ">
       <div className="flex h-[100%] w-[80%]  flex-row items-center justify-between  text-center">
         <Link href="/home" className="flex h-[36px] flex-row items-end justify-end text-end">
-          <p className="nav-text-logo"> 2D/G/TS</p>
+          <p className="nav-text-logo">
+            2D<i>I</i>G<i>I</i>TS
+          </p>
           <p className="w-[50px] text-start text-[5px]">DEVELOPMENT AGENCY</p>
         </Link>
         <div className=" flex flex-row gap-[32px] leading-[24px] text-white">
--- src/app/selected/page.tsx
+++ src/app/selected/page.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import Badge from '../shared/badge';
+import { HeroComponent } from '../shared/heroComponent';
+import { blogsInputs } from '../types/blogsInputs';
+
 export default function BlogPost() {
   return (
-    <>
-      <div>
-        <p>we got </p>
+    <div className="flex  flex-col justify-center gap-[62px]">
+      <div className="mt-[1px] flex h-[300px]  flex-col items-center justify-center gap-2 overflow-hidden  text-center">
+        <HeroComponent heroInputs={blogsInputs} />
       </div>
-    </>
+      <Badge Input="INTERVIEW" />
+    </div>
   );
 }
