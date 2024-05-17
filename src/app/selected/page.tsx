1897ed398a87839deedd53a0f18b822565d23448
Ameer Ameen <ameerapex496@gmail.com>
2024-05-17 22:44:24 +0200

    added styling to selected blog, and margin and paddings

--- src/app/selected/page.tsx
+++ src/app/selected/page.tsx
@@ -6,11 +6,23 @@ import { blogsInputs } from '../types/blogsInputs';
 
 export default function BlogPost() {
   return (
-    <div className="flex  flex-col justify-center gap-[62px]">
+    <div>
       <div className="mt-[1px] flex h-[300px]  flex-col items-center justify-center gap-2 overflow-hidden  text-center">
         <HeroComponent heroInputs={blogsInputs} />
       </div>
-      <Badge Input="INTERVIEW" />
+      <div className=" flex h-[1000px] w-[100%]  flex-col items-center justify-start bg-white text-start">
+        <div className=" relative mt-[62px] h-[40px]  w-[80%] bg-white ">
+          <Badge Input="INTERVIEW" />
+        </div>
+
+        <div className=" w-[80%]">
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ullam accusantium
+            dicta. Consectetur repellat illum, incidunt laudantium sequi aliquid quo autem quis
+            optio quas voluptatem obcaecati impedit! Repellendus, dolorum vero.
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
